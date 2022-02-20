0cde928bbd7d417736423ce8f7847f4c1def4346
thanhpromus <40162069+thanhpromus@users.noreply.github.com>
2022-02-20 10:26:28 +0700

    Update CONFIG.js

--- CONFIG.js
+++ CONFIG.js
@@ -7,5 +7,4 @@ const CONFIGDATA = {
   titleModar: "Cảm ơn em vì đồng ý",
   descriptionModar:
     'Tớ biết là cậu sẽ đồng ý làm người yêu tớ mà ^^ </br> Cám ơn cậu đã đồng ý để cho tớ được che chở và bảo vệ cậu. Đối với tớ, cậu mãi là cô bé :)) </br> "Thích cậu nhiều đến mấy, cậu sẽ hiểu thôi. </br>  Tớ thích cậu nhiều đến mấy, cậu sẽ hiểu thôi "',
-
-Rechapt: Thành Promus ("0o0)};
+};
